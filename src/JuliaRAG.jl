bd8a3e30c499eaeb6e772fe99af028434b0bed08
StirlingSmith <stirlingjamessmith@gmail.com>
2023-12-12 13:50:27 +1300

    minimum viable package

--- src/JuliaRAG.jl
+++ src/JuliaRAG.jl
@@ -2,4 +2,23 @@ module JuliaRAG
 
 # Write your package code here.
     using PythonCall
+    using CondaPkg
+    using Distances
+    using NearestNeighbors
+
+    const pyglobals = PyDict()
+    
+    include("TextProcessing/get_embedding.jl")
+    include("TextProcessing/parse_pdf.jl")
+
+    include("embed_pdf.jl")
+    include("make_vector_space.jl")
+    include("query_llm.jl")
+
+    export embed_pdf
+    export query_llm
+
 end
+
+
+
