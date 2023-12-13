ded4a2040df37fe1af8caffbc3f3ddf7fd2d3752
StirlingSmith <stirlingjamessmith@gmail.com>
2023-12-14 09:05:24 +1300

    add option for k

--- src/query_llm.jl
+++ src/query_llm.jl
@@ -4,11 +4,12 @@ Creates and executes a query to an LLM.
 # Arguments
 - `query::String`: The question and context to be answered in the query.
 - `search_space::Tuple`: Vector space and associated chunkised text.
+- `k::Int`: The number of nearest neighbours required
 """
-function query_llm(query::String, search_space::Tuple)
+function query_llm(query::String, search_space::Tuple, k::Int)
     query_embedding = get_embedding(query)
 
-    idx, _ = knn(search_space[1], query_embedding, 3)
+    idx, _ = knn(search_space[1], query_embedding, k)
     relevant_chunks = search_space[2][idx]
 
     relevant_text = join(relevant_chunks, "\n")
