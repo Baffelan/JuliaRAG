5c3d58c7e7fa179e1bd8c769fcdd2208194e710b
StirlingSmith <stirlingjamessmith@gmail.com>
2023-12-15 09:34:31 +1300

    embed string function added

--- src/JuliaRAG.jl
+++ src/JuliaRAG.jl
@@ -9,12 +9,15 @@ module JuliaRAG
     include("TextProcessing/get_embedding.jl")
     include("TextProcessing/parse_pdf.jl")
 
+    
     include("embed_pdf.jl")
+    include("embed_str.jl")
     include("make_vector_space.jl")
     include("query_llm.jl")
     include("install_python_packs.jl")
 
     export embed_pdf
+    export embed_str
     export query_llm
     export install_python_packs
 
---
+++ src/embed_str.jl
@@ -0,0 +1,22 @@
+"""
+Embeds a text in RAM for vector searching.
+
+# Arguments
+- `text::String`: Text to be embedded
+"""
+function embed_str(text::String)
+    splitter = pyimport("langchain.text_splitter")
+    text_splitter = splitter.RecursiveCharacterTextSplitter(
+        # Set a really small chunk size, just to show.
+        chunk_size = 1000,
+        chunk_overlap  = 100,
+        length_function = length,
+        add_start_index = true,
+    )
+    texts = pyconvert.(Vector, text_splitter.create_documents([pystr(text)]))
+
+    strings = getindex.(getindex.(texts,1),2)
+    embeddings = get_embedding.(strings)
+    
+    search_space = make_vector_space(strings, embeddings)
+end
